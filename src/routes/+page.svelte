94d1ac3c4363f8d6a6944c62a6534419159d9ac2
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-01-15 15:35:54 +0900

    add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<div class="home">
+  <h1>Tako Board</h1>
+  <div class="version">Ver 0.5.0</div>
+  <p>ちゃこちゃんのお世話管理アプリ</p>
+  <img alt="tako" src={`${base}/img/tako.png`} />
+</div>
+
+<style>
+  .home .version {
+    color: #999999;
+    font-size: 0.9rem;
+  }
+  .home > img {
+    margin: 15px auto;
+    width: 50%;
+  }
+</style>

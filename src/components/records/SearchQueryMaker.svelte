f9589d8e53afa9155b302c033b1c2adde180b76d
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-01-16 09:34:23 +0900

    fix style

--- src/components/records/SearchQueryMaker.svelte
+++ src/components/records/SearchQueryMaker.svelte
@@ -5,7 +5,7 @@
   import { recordTypeStrList } from "$lib/records";
   import { onMount } from "svelte";
   import DatePicker from "./DatePicker.svelte";
-  // import DateSelectorVue from "@/components/DateSelector.vue";
+  import ToggleButton from "../ToggleButton.svelte";
 
   const today = new Date();
   const tempToday = new Date();
@@ -13,15 +13,15 @@
 
   $: endDate = toDateString(today);
   $: startDate = toDateString(prevWeekDay);
-  $: boxOpening = false;
+  $: active = false;
   $: fetchButtonDisabled = true;
-  $: recordType = "-1";
+  $: recordType = -1;
 
   export let fetchCallback: (queries: QueryConstraint[]) => Promise<void>;
 
   onMount(() => fetch());
 
-  const toggleBoxOpening = () => (boxOpening = !boxOpening);
+  const toggleActive = () => (active = !active);
 
   const fetch = async () => {
     // 取得時期期間, 検索条件等を総合したクエリを作成し, propsで受け取っているコールバックに渡しながら実行
@@ -31,8 +31,8 @@
       endAt(new Date(startDate)),
       startAt(new Date(endDate)),
     ];
-    if (recordType !== "-1") {
-      queries.push(where("type", "==", parseInt(recordType)));
+    if (recordType !== -1) {
+      queries.push(where("type", "==", recordType));
     }
     fetchButtonDisabled = true;
     await fetchCallback(queries);
@@ -54,24 +54,24 @@
 
 <!-- Firestore検索クエリを作成し取得ボタンを押したらコールバックに渡すコンポーネント -->
 <div class="query-maker">
-  {#if !boxOpening}
-    <button class="toggle-button" on:click={toggleBoxOpening}>+</button>
+  <ToggleButton {active} on:click={toggleActive} />
+  {#if !active}
+    <!-- <button class="toggle-button" on:click={toggleActive}>+</button> -->
     <span>検索ボックス</span>
   {:else}
-    <button class="toggle-button" on:click={toggleBoxOpening}>-</button>
+    <!-- <button class="toggle-button" on:click={toggleActive}>-</button> -->
     <div class="input-box">
       <h4>期間指定</h4>
       <DatePicker on:end-date-changed={endDateChanged} on:start-date-changed={startDateChanged} />
-      <div>
-        <h4>タイプ指定</h4>
-        <select name="query-record-type" bind:value={recordType} on:change={recordTypeChanged}>
-          <option value="-1">---</option>
-          <!-- valueをバインドする際は-1に合わせるためstringに変換している. -->
-          {#each recordTypeStrList as recordTypeStr, index (recordTypeStr)}
-            <option value={"" + index}>{recordTypeStr}</option>
-          {/each}
-        </select>
-      </div>
+
+      <h4>タイプ指定</h4>
+      <select name="query-record-type" bind:value={recordType} on:change={recordTypeChanged}>
+        <option value={-1}>---</option>
+        {#each recordTypeStrList as recordTypeStr, index (recordTypeStr)}
+          <option value={index}>{recordTypeStr}</option>
+        {/each}
+      </select>
+
       <button class="search-execute" on:click|preventDefault={fetch} disabled={fetchButtonDisabled}>
         取得
       </button>
@@ -85,19 +85,16 @@
     margin: 0.2rem auto 2.8rem;
     padding: 1rem 0.6rem;
   }
-  .toggle-button {
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
-    line-height: 1rem;
-    border: 1px solid #000000;
-    cursor: pointer;
-  }
   span {
     margin: 0 0.5rem;
   }
   h4 {
-    margin: auto;
+    margin: 1.5rem auto;
+  }
+  .input-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   .search-execute {
     margin-top: 0.6rem;
